3937c40d2e8f53d3145837c0e0c9406e804f1330
sp00nman <fiorella.schischlik@gmail.com>
2020-05-12 18:06:52 -0400

    modifications to figure 2a

--- Chung2017/src/plot_figure2a.R
+++ Chung2017/src/plot_figure2a.R
@@ -2,16 +2,23 @@ library(dplyr)
 library(scater)
 library(scran)
 library(ggplot2)
+library(ggforce)
 library(ggpubr)
 
 # use below link as guide
 # http://www.sthda.com/english/articles/24-ggpubr-publication-ready-plots/81-ggplot2-easy-way-to-mix-multiple-graphs-on-the-same-page/
 
-genus.counts <- read.table(snakemake@input[[1]], sep="\t", header=TRUE, row.names=1)
-species.counts <- read.table(snakemake@input[[2]], sep="\t", header=TRUE, row.names=1)
+setwd("~/src/CSI-Microbes-analysis/Chung2017/")
+
+#genus.counts <- read.table(snakemake@input[[1]], sep="\t", header=TRUE, row.names=1)
+#species.counts <- read.table(snakemake@input[[2]], sep="\t", header=TRUE, row.names=1)
+genus.counts <- read.table("output/BC06_genus_PathSeq_microbe_reads.tsv", sep="\t", header=TRUE, row.names=1)
+species.counts <- read.table("output/BC06_species_PathSeq_microbe_reads.tsv", sep="\t", header=TRUE, row.names=1)
 # counts <- read.table("output/Pt0_genus_PathSeq_microbe_reads.tsv", sep="\t", header=TRUE, row.names=1)
-genus.pdata <- read.table(snakemake@input[[3]], sep="\t", header = TRUE, row.names=1)
-species.pdata <- read.table(snakemake@input[[3]], sep="\t", header = TRUE, row.names=1)
+#genus.pdata <- read.table(snakemake@input[[3]], sep="\t", header = TRUE, row.names=1)
+#species.pdata <- read.table(snakemake@input[[3]], sep="\t", header = TRUE, row.names=1)
+genus.pdata <- read.table("output/BC06_genus_PathSeq_metadata.tsv", sep="\t", header=TRUE, row.names=1)
+species.pdata <- read.table("output/BC06_species_PathSeq_metadata.tsv", sep="\t", header=TRUE, row.names=1)
 
 # plot genus level finding
 min.reads <- 2
@@ -23,9 +30,28 @@ genus.sce <- computeSumFactors(genus.sce, clusters=celltype)
 genus.sce <- logNormCounts(genus.sce)
 
 # Figure 2A1 - expression of Steptomyces
-fig.2A1 <- plotExpression(genus.sce, x="celltype1",
-    features="Streptomyces") +
-    theme(axis.text.x = element_text(angle = 90))
+feature <- "Streptomyces"
+sce_logcounts <- genus.sce@assays@data$logcounts
+plot_fig2 <- data.frame(
+    logcounts = sce_logcounts[feature, ],
+    cell_types = genus.sce$celltype1)
+
+fig.2A1 <- ggplot(plot_fig2, aes(x=cell_types, y=logcounts)) + 
+    geom_violin(scale="width", adjust=0.7, trim=TRUE) +
+    geom_sina(aes(x = cell_types, 
+                  y = logcounts, 
+                  fill = cell_types),
+              alpha=0.6,
+              size = 3,
+              pch=21,
+              scale=FALSE) +
+    ylim(c(0,13)) +
+    scale_fill_manual(values=c("#e74c3c", "#3498db", "#2ecc71")) +
+    ylab("Expression log(counts)") +
+    xlab("Cells") +
+    ggtitle(feature) +
+    theme_pubr(legend="none")
+plot(fig.2A1)
 
 # plot species level findings
 min.reads <- 2
@@ -37,15 +63,40 @@ species.sce <- computeSumFactors(species.sce, clusters=celltype)
 species.sce <- logNormCounts(species.sce)
 
 # Figure 2A2 - expression of Streptomyces novaecaesareae
-fig.2A2 <- plotExpression(species.sce, x="celltype1",
-    features="Streptomyces_novaecaesareae") +
-    theme(axis.text.x = element_text(angle = 90))
+feature <- "Streptomyces_novaecaesareae"
+sce_logcounts <- species.sce@assays@data$logcounts
+plot_fig2 <- data.frame(
+    logcounts = sce_logcounts[feature, ],
+    cell_types = genus.sce$celltype1)
+
+fig.2A2 <- ggplot(plot_fig2, aes(x=cell_types, y=logcounts)) + 
+    geom_violin(scale="width", trim=TRUE) +
+    geom_jitter(aes(x=cell_types, y=logcounts, fill=celltype), pch=21, alpha=0.6, size=3) +
+    scale_fill_manual(values=c("#e74c3c", "#3498db", "#2ecc71")) +
+    ylab("Expression log(counts)") +
+    xlab("Cells") +
+    ggtitle("Streptomyces novaecaesareae") +
+    theme_pubr(legend="none")
+plot(fig.2A2)
 
 # Figure 2A3 - expression of Streptomyces specialis
-fig.2A3 <- plotExpression(species.sce, x="celltype1",
-    features="Streptomyces_specialis") +
-    theme(axis.text.x = element_text(angle = 90))
+feature <- "Streptomyces_specialis"
+sce_logcounts <- species.sce@assays@data$logcounts
+plot_fig2 <- data.frame(
+    logcounts = sce_logcounts[feature, ],
+    cell_types = genus.sce$celltype1)
+
+fig.2A3 <- ggplot(plot_fig2, aes(x=cell_types, y=logcounts)) + 
+    geom_violin(scale="width", trim=TRUE) +
+    geom_jitter(aes(x=cell_types, y=logcounts, fill=celltype), pch=21, alpha=0.6, size=3) +
+    scale_fill_manual(values=c("#e74c3c", "#3498db", "#2ecc71")) +
+    ylab("Expression log(counts)") +
+    xlab("Cells") +
+    ggtitle("Streptomyces specialis") +
+    theme_pubr(legend="none")
+plot(fig.2A3)
 
 ggarrange(fig.2A1, fig.2A2, fig.2A3, ncol=3, nrow=1, labels=c("A", "", ""))
+#ggsave(filename="fig2.pdf", path="~/src/CSI-Microbes-analysis/Chung2017/output/", width=11, height=5)
 
-ggsave(snakemake@output[[1]], width=11, height=7)
+ggsave(snakemake@output[[1]], width=11, height=5)
